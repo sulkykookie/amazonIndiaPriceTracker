45de6a4b3ccd12051140f826d0e0a3802e091b77
Shrey Kumar Singh <shrey18266@iiitd.ac.in>
2020-02-28 18:25:33 +0530

    Updated Comments

--- TrackPrice.py
+++ TrackPrice.py
@@ -17,7 +17,6 @@ import requests
 currentDT = datetime.datetime.now()
 
 #Amazon URL goes here
-#URL = "https://www.amazon.in/Sony-A6000Y-Digital-16-50mm-55-210mm/dp/B00XOXFB1I/ref=sr_1_3?keywords=sony+a6000&qid=1562674094&s=electronics&sr=1-3"
 URL = "https://www.amazon.in/Sony-A6000Y-Digital-16-50mm-55-210mm/dp/B00XOXFB1I/ref=asc_df_B00XOXFB1I/?tag=googleshopdes-21&linkCode=df0&hvadid=396986111700&hvpos=&hvnetw=g&hvrand=8647138198519415733&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1007751&hvtargid=pla-352077303119&psc=1&ext_vrnc=hi"
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'}
@@ -25,10 +24,13 @@ page = requests.get(URL, headers=headers)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 #variables from html elements on the URL
-title = soup.find(id = "productTitle").get_text()
-filename = title.strip()+".csv"
-price = soup.find(id = "priceblock_ourprice").get_text()
-price = price[2:]
+title = soup.find(id = "productTitle").get_text()	#Fetches the name of the product by finding span with tage "productTitle"
+
+filename = title.strip()+".csv"						#Creates new .csv file for the spcific product
+
+price = soup.find(id = "priceblock_ourprice").get_text()	#Fetches the price of the product by finding span with tage "priceblock_ourprice"
+price = price[2:]											#Removes the Rupee symbol
+
 date = currentDT.strftime("%d/%m/%Y")
 time = currentDT.strftime("%H:%M:%S")
 
